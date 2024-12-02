edd2bdd10078116592f683db079dc2e793a6ff39
tim9810 <tesla9810@gmail.com>
2024-12-02 14:58:09 +0800

    Update 6_🚕🚕_nyc_uber.py
    split tif

--- pages/6_🚕🚕_nyc_uber.py
+++ pages/6_🚕🚕_nyc_uber.py
@@ -1,44 +1,36 @@
+pip install streamlit leafmap
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-st.title("Interactive Map with GeoTIFF")
+st.set_page_config(layout="wide")
 
-# 檢查必要模組是否已安裝
-try:
-    import xarray
-    import rioxarray
-except ImportError as e:
-    st.error(f"缺少必要模組: {e}. 請運行以下命令來安裝依賴: pip install xarray rioxarray rasterio")
-    st.stop()
+st.sidebar.title("衛星影像分割展示")
+st.sidebar.info(
+    """
+    - 分析兩個衛星影像，進行分割展示
+    - 檔案來源: GitHub 儲存庫
+    """
+)
 
-# GeoTIFF 檔案路徑或網址
-tif_url = "https://github.com/tim9810/data2/blob/main/LC09_117042_20230114.tif"
+st.title("Split-panel Map for Satellite Imagery")
 
-# 設定互動式地圖
-col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
+# 衛星影像文件 URL
+url1 = "https://raw.githubusercontent.com/tim9810/data2/main/LC09_117042_20230114.tif"
+url2 = "https://raw.githubusercontent.com/tim9810/data2/main/LC09_117042_20230130.tif"
 
-with col2:
-    # 讓使用者選擇底圖
-    basemap = st.selectbox("Select a basemap:", options, index)
+st.sidebar.info(
+    f"""
+    左圖: {url1.split('/')[-1]}  
+    右圖: {url2.split('/')[-1]}
+    """
+)
 
-with col1:
-    # 創建地圖
-    m = leafmap.Map(
-        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-    )
-    m.add_basemap(basemap)
+# 創建地圖對象
+m = leafmap.Map()
+m.split_map(left_layer=url1, right_layer=url2)
 
-    # 加載 GeoTIFF 檔案
-    try:
-        m.add_raster(
-            tif_url,
-            layer_name="GeoTIFF Layer",
-            opacity=0.7,
-        )
-        m.to_streamlit(height=700)
-    except Exception as e:
-        st.error(f"無法加載 GeoTIFF 檔案: {e}")
+# 顯示地圖
+m.to_streamlit(height=700)
+
 
 
