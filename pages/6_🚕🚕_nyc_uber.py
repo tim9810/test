c9250550197ceefda917cca72d9ba81a1caa0541
tim9810 <tesla9810@gmail.com>
2024-12-02 14:43:00 +0800

    Update 6_🚕🚕_nyc_uber.py
    tif hw1127

--- pages/6_🚕🚕_nyc_uber.py
+++ pages/6_🚕🚕_nyc_uber.py
@@ -1,20 +1,10 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-# 側邊欄的資訊
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+st.title("Interactive Map with GeoTIFF")
 
-st.title("Interactive Map with GeoJSON")
-
-# GeoJSON 資料連結 (從你的 GitHub 儲存庫提供)
-geojson_url = "https://github.com/tim9810/data2/blob/main/Export_Output.geojson"
+# GeoTIFF 檔案路徑或網址
+tif_url = "https://github.com/tim9810/data2/blob/main/LC09_117042_20230114.tif"
 
 # 設定互動式地圖
 col1, col2 = st.columns([4, 1])
@@ -32,18 +22,14 @@ with col1:
     )
     m.add_basemap(basemap)
 
-    # 加載 GeoJSON 資料
-    m.add_geojson(
-        geojson_url,
-        layer_name="Taiwan Population",
-        style={
-            "fillColor": "#FF8F59",
-            "color": "black",
-            "weight": 0.5,
-            "dashArray": "5, 5",
-            "fillOpacity": 0.6,
-        },
+    # 加載 GeoTIFF 檔案
+    m.add_cog_layer(
+        tif_url,
+        name="GeoTIFF Layer",
+        opacity=0.7,
+        fit_bounds=True,
     )
 
     # 在 Streamlit 中顯示地圖
     m.to_streamlit(height=700)
+
