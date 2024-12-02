0e0f08ce3d111148e1420f31142966fd82b05092
tim9810 <tesla9810@gmail.com>
2024-12-02 15:33:02 +0800

    Update 6_split Landsat 9 NDVI.py

--- pages/6_split Landsat 9 NDVI.py
+++ pages/6_split Landsat 9 NDVI.py
@@ -1,37 +1,5 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-with st.expander("See source code"):
-    with st.echo():
-st.set_page_config(layout="wide")
-
-st.sidebar.title("Landsat 9 NDVI分割展示")
-st.sidebar.info(
-    """
-    - Landsat 9 NDVI影像，進行分割展示
-    - 檔案來源: GEE檔案放入GitHub 
-    """
-)
-
-st.title("Landsat 9 NDVI分割展示 以半個台灣為例")
-
-# 衛星影像文件 URL
-url1 = "https://raw.githubusercontent.com/tim9810/data2/main/LC09_117043_20230826.tif"
-url2 = "https://raw.githubusercontent.com/tim9810/data2/main/LC09_117043_20230911.tif"
-
-st.sidebar.info(
-    f"""
-    左圖: {url1.split('/')[-1]}  
-    右圖: {url2.split('/')[-1]}
-    """
-)
-
-# 創建地圖對象
-m = leafmap.Map()
-m.split_map(left_layer=url1, right_layer=url2)
-
-# 顯示地圖
-m.to_streamlit(height=700)
-
 
 st.set_page_config(layout="wide")
 
