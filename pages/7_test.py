c0f1eb14a5236846c4825727b1c960342efbaa92
tim9810 <tesla9810@gmail.com>
2024-12-11 16:20:48 +0800

    Update 7_test.py

--- pages/7_test.py
+++ pages/7_test.py
@@ -20,7 +20,7 @@ with st.expander("See source code"):
 
         m = leafmap.Map(center=[40, -100], zoom=4)
         cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+        regions = "https://raw.github.com/tim9810/gis_final_exam/blob/main/tainung/tainung.geojson"
 
         m.add_geojson(regions, layer_name="US Regions")
         m.add_points_from_xy(
